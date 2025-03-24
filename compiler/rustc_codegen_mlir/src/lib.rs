41fbff2d947fbfb156e106d9427a71326ca75eb5
Sawyer Bergeron <sawyerbergeron@gmail.com>
2025-03-24 16:09:06 -0400

    consulting the experts
    Signed-off-by: Sawyer Bergeron <sawyerbergeron@gmail.com>

--- compiler/rustc_codegen_mlir/src/lib.rs
+++ compiler/rustc_codegen_mlir/src/lib.rs
@@ -6,144 +6,20 @@ use rustc_codegen_ssa::{CodegenResults, CompiledModule, ModuleCodegen};
 use rustc_data_structures::fx::FxIndexMap;
 use rustc_errors::ErrorGuaranteed;
 use rustc_metadata::EncodedMetadata;
-use rustc_middle::dep_graph::{WorkProduct, WorkProductId};
+use rustc_middle::dep_graph::{self, WorkProduct, WorkProductId};
 use rustc_middle::infer::canonical::ir::ConstKind::Value;
-use rustc_middle::ty::TyCtxt;
+use rustc_middle::ty::{self, Ty, TyCtxt};
 use rustc_session::Session;
 use rustc_session::config::OutputFilenames;
 
 rustc_fluent_macro::fluent_messages! { "../messages.ftl" }
 
+mod base;
+
 #[derive(Clone)]
 pub struct MLIRCodegenBackend(());
 
-impl ExtraBackendMethods for MLIRCodegenBackend {
-    fn codegen_allocator<'tcx>(
-        &self,
-        tcx: TyCtxt<'tcx>,
-        module_name: &str,
-        kind: rustc_ast::expand::allocator::AllocatorKind,
-        alloc_error_handler_kind: rustc_ast::expand::allocator::AllocatorKind,
-    ) -> Self::Module {
-        todo!()
-    }
-
-    fn compile_codegen_unit(
-        &self,
-        tcx: TyCtxt<'_>,
-        cgu_name: rustc_span::Symbol,
-    ) -> (ModuleCodegen<Self::Module>, u64) {
-        todo!()
-    }
-
-    fn target_machine_factory(
-        &self,
-        sess: &Session,
-        opt_level: rustc_session::config::OptLevel,
-        target_features: &[String],
-    ) -> rustc_codegen_ssa::back::write::TargetMachineFactoryFn<Self> {
-        todo!()
-    }
-}
-
-impl WriteBackendMethods for MLIRCodegenBackend {
-    type Module = ();
-
-    type TargetMachine = ();
-
-    type TargetMachineError = ();
-
-    type ModuleBuffer;
-
-    type ThinData = ();
-
-    type ThinBuffer;
-
-    fn run_link(
-        cgcx: &rustc_codegen_ssa::back::write::CodegenContext<Self>,
-        dcx: rustc_errors::DiagCtxtHandle<'_>,
-        modules: Vec<ModuleCodegen<Self::Module>>,
-    ) -> Result<ModuleCodegen<Self::Module>, rustc_errors::FatalError> {
-        todo!()
-    }
-
-    fn run_fat_lto(
-        cgcx: &rustc_codegen_ssa::back::write::CodegenContext<Self>,
-        modules: Vec<rustc_codegen_ssa::back::write::FatLtoInput<Self>>,
-        cached_modules: Vec<(
-            rustc_codegen_ssa::back::lto::SerializedModule<Self::ModuleBuffer>,
-            WorkProduct,
-        )>,
-    ) -> Result<rustc_codegen_ssa::back::lto::LtoModuleCodegen<Self>, rustc_errors::FatalError>
-    {
-        todo!()
-    }
-
-    fn run_thin_lto(
-        cgcx: &rustc_codegen_ssa::back::write::CodegenContext<Self>,
-        modules: Vec<(String, Self::ThinBuffer)>,
-        cached_modules: Vec<(
-            rustc_codegen_ssa::back::lto::SerializedModule<Self::ModuleBuffer>,
-            WorkProduct,
-        )>,
-    ) -> Result<
-        (Vec<rustc_codegen_ssa::back::lto::LtoModuleCodegen<Self>>, Vec<WorkProduct>),
-        rustc_errors::FatalError,
-    > {
-        todo!()
-    }
-
-    fn print_pass_timings(&self) {
-        todo!()
-    }
-
-    fn print_statistics(&self) {
-        todo!()
-    }
-
-    unsafe fn optimize(
-        cgcx: &rustc_codegen_ssa::back::write::CodegenContext<Self>,
-        dcx: rustc_errors::DiagCtxtHandle<'_>,
-        module: &ModuleCodegen<Self::Module>,
-        config: &rustc_codegen_ssa::back::write::ModuleConfig,
-    ) -> Result<(), rustc_errors::FatalError> {
-        todo!()
-    }
-
-    fn optimize_fat(
-        cgcx: &rustc_codegen_ssa::back::write::CodegenContext<Self>,
-        llmod: &mut ModuleCodegen<Self::Module>,
-    ) -> Result<(), rustc_errors::FatalError> {
-        todo!()
-    }
-
-    unsafe fn optimize_thin(
-        cgcx: &rustc_codegen_ssa::back::write::CodegenContext<Self>,
-        thin: rustc_codegen_ssa::back::lto::ThinModule<Self>,
-    ) -> Result<ModuleCodegen<Self::Module>, rustc_errors::FatalError> {
-        todo!()
-    }
-
-    unsafe fn codegen(
-        cgcx: &rustc_codegen_ssa::back::write::CodegenContext<Self>,
-        dcx: rustc_errors::DiagCtxtHandle<'_>,
-        module: ModuleCodegen<Self::Module>,
-        config: &rustc_codegen_ssa::back::write::ModuleConfig,
-    ) -> Result<CompiledModule, rustc_errors::FatalError> {
-        todo!()
-    }
-
-    fn prepare_thin(
-        module: ModuleCodegen<Self::Module>,
-        want_summary: bool,
-    ) -> (String, Self::ThinBuffer) {
-        todo!()
-    }
-
-    fn serialize_module(module: ModuleCodegen<Self::Module>) -> (String, Self::ModuleBuffer) {
-        todo!()
-    }
-}
+struct CodegenCarrier {}
 
 impl CodegenBackend for MLIRCodegenBackend {
     fn locale_resource(&self) -> &'static str {
@@ -158,13 +34,32 @@ impl CodegenBackend for MLIRCodegenBackend {
     ) -> Box<dyn std::any::Any> {
         println!("doing crate codegen");
 
-        Box::new(rustc_codegen_ssa::base::codegen_crate(
-            MLIRCodegenBackend(()),
-            tcx,
-            rustc_codegen_llvm::llvm_util::target_cpu(tcx.sess).to_string(),
-            metadata,
-            need_metadata_module,
-        ))
+        let (defids, codegen_units) = tcx.collect_and_partition_mono_items(());
+
+        if tcx.dep_graph.is_fully_enabled() {
+            for cgu in codegen_units {
+                // TODO: do I need to use `ensure()`? since demand should be sufficient, no?
+                //let c1 = tcx.codegen_unit(cgu.name());
+                tcx.ensure().codegen_unit(cgu.name());
+            }
+        } else {
+            println!("dep graph not fully enabled");
+        }
+
+        for (i, cgu) in codegen_units.iter().enumerate() {
+            let s = cgu.name();
+            println!("looking at cgu: {s}");
+            let dep_node = cgu.codegen_dep_node(tcx);
+            let module = tcx.dep_graph.with_task(
+                dep_node,
+                tcx,
+                cgu.name(),
+                base::module_codegen,
+                Some(dep_graph::hash_result),
+            );
+        }
+
+        todo!()
     }
 
     fn join_codegen(
@@ -175,12 +70,7 @@ impl CodegenBackend for MLIRCodegenBackend {
     ) -> (CodegenResults, FxIndexMap<WorkProductId, WorkProduct>) {
         println!("doing join codegen");
 
-        let (codegen_results, work_products) = ongoing_codegen
-            .downcast::<rustc_codegen_ssa::back::write::OngoingCodegen<MLIRCodegenBackend>>()
-            .expect("Expected MLIRCodegenBackend's OngoingCodegen, found Box<Any>")
-            .join(sess);
-
-        (codegen_results, work_products)
+        todo!()
     }
 
     fn link(
@@ -191,7 +81,7 @@ impl CodegenBackend for MLIRCodegenBackend {
     ) -> Result<(), ErrorGuaranteed> {
         println!("doing link codegen");
 
-        use rustc_codegen_ssa::back::link::link_binary;
+        //use rustc_codegen_ssa::back::link::link_binary;
 
         //use crate::back::archive::LlvmArchiveBuilderBuilder;
 
